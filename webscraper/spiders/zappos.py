0297dfba529cfb2bc8cf262f8d08e69807e2eee5
pierreville <pierreville@gmail.com>
2021-12-13 14:50:12 -0500

    Add Zappos scraper

---
+++ webscraper/spiders/zappos.py
@@ -0,0 +1,21 @@
+import scrapy
+
+class ZapposSpider(scrapy.Spider):
+    name = "zappos"
+    store_url = "https://www.zappos.com"
+    start_urls = [
+        'https://www.zappos.com/volleyball-shoes'
+    ]
+
+    def parse(self, response):
+        for item in response.css('div#searchPage article'):
+            product_name = item.css('dd[itemprop="name"]::text').extract_first(),
+            brand_name = item.css('dd[itemprop="brand"]::text').extract_first(),
+            url = response.urljoin(item.css('a::attr(href)').extract_first()),
+
+            whole_name = brand_name[0] + ' ' + product_name[0]
+            
+            yield {
+                'aff_title': whole_name,
+                'aff_url': url,
+            }
