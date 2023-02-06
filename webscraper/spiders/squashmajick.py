de7336daadfceb1c3cb93ed7ad852d84ee130a3e
pierreville <pierreville@gmail.com>
2023-02-06 13:35:37 -0500

    squashmajick

---
+++ webscraper/spiders/squashmajick.py
@@ -0,0 +1,22 @@
+import scrapy
+
+class squashmajick(scrapy.Spider):
+    name = "squashmajick"
+    start_urls = [
+        'https://www.ebay.com.au/sch/i.html?_nkw=&_armrs=1&_ipg=&_from=&_ssn=squashmajik&_sop=10',
+    ]
+
+    def parse(self, response):
+        for item in response.css('div.srp-river-results div.s-item__wrapper'):
+
+            yield {
+                'aff_url': item.css('div.s-item__info a::attr(href)').get(),
+                'aff_title': item.css('div.s-item__info div.s-item__title span::text').get()
+                'aff_id': item.css('div.s-item__image-section a::attr(data-id)').get()
+            }
+
+        #next_page = response.css('div.pagination span.next a::attr(href)').get()
+        
+        #if next_page is not None:
+        #    next_page = response.urljoin(next_page)
+        #    yield scrapy.Request(next_page, callback=self.parse)
