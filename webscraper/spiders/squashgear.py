14c7d6cc550d776a98ef27b2988d811c10819577
pierreville <pierreville@gmail.com>
2023-09-01 11:27:54 -0400

    SquasGear scrapy update from switch to Shopify

--- webscraper/spiders/squashgear.py
+++ webscraper/spiders/squashgear.py
@@ -3,27 +3,7 @@ import scrapy
 class SquashGearSpider(scrapy.Spider):
     name = "squashgear"
     start_urls = [
-        'https://www.squashgear.com/collections/dunlop-squash-rackets',
-        'https://www.squashgear.com/collections/tecnifibre-squash-rackets',
-        'https://www.squashgear.com/collections/prince-squash-rackets',
-        'https://www.squashgear.com/collections/eye-squash-rackets',
-        'https://www.squashgear.com/collections/wilson-squash-rackets',
-        'https://www.squashgear.com/collections/harrow-squash-rackets',
-        'https://www.squashgear.com/collections/head-squash-rackets',
-        'https://www.squashgear.com/collections/black-knight-squash-rackets',
-        'https://www.squashgear.com/collections/karakal-squash-rackets',
-        'https://www.squashgear.com/collections/manta-squash-rackets',
-        'https://www.squashgear.com/collections/unsquashable-squash-rackets',
-        'https://www.squashgear.com/collections/asics-squash-shoes',
-        'https://www.squashgear.com/collections/eye-squash-shoes',
-        'https://www.squashgear.com/collections/prince-squash-shoes',
-        'https://www.squashgear.com/collections/yonex-squash-shoes',
-        'https://www.squashgear.com/collections/salming-squash-shoes',
-        'https://www.squashgear.com/collections/adidas-squash-shoes',
-        'https://www.squashgear.com/collections/harrow-squash-shoes',
-        'https://www.squashgear.com/collections/wilson-squash-shoes',
-        'https://www.squashgear.com/collections/hi-tec-squash-shoes',
-        'https://www.squashgear.com/collections/junior-squash-shoes',
+        'https://www.squashgear.com/collections/all',
     ]
 
     def parse(self, response):
@@ -32,3 +12,8 @@ class SquashGearSpider(scrapy.Spider):
                 'aff_url': response.urljoin(item.css('a::attr(href)').extract_first()),
                 'aff_title': item.css('div.prod-title::text').extract_first(),
             }
+
+        next_page = response.urljoin(response.css('div.pagination a[title="Next page"]::attr(href)').extract_first())
+        
+        if next_page is not None:
+            yield scrapy.Request(next_page, callback=self.parse)
